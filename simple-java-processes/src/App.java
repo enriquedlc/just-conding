fa02f31370ccf31e51dabd159358be922efea4db
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-10-16 23:57:11 +0100

    having fun!

--- simple-java-processes/src/App.java
+++ simple-java-processes/src/App.java
@@ -1,5 +1,30 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String filePath = "C:\\Users\\Enrique\\Desktop\\Proyectos\\just-coding\\simple-java-processes\\src\\files\\out.txt";
+
+        String contentToWrite = "to the out.txt file";
+
+        try {
+            File file = new File(filePath);
+
+            if (!file.exists())
+                file.createNewFile();
+
+            FileOutputStream fos = new FileOutputStream(file);
+
+            byte[] contentInBytes = contentToWrite.getBytes();
+
+            fos.write(contentInBytes);
+            fos.flush();
+            fos.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
