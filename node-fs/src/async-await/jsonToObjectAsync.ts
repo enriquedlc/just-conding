ac351e9d5a73a4a1759f0fc55f0703fb8d6c2809
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-11-01 23:24:42 +0000

    :hammer: try catch block on jsonToObjectoAsync function

--- node-fs/src/async-await/jsonToObjectAsync.ts
+++ node-fs/src/async-await/jsonToObjectAsync.ts
@@ -1,6 +1,10 @@
 import fs from "node:fs/promises";
 
 export const jsonToObjectAsync = async (jsonFilePath: string) => {
-    const jsonString = await fs.readFile(jsonFilePath, "utf-8");
-    return JSON.parse(jsonString);
+    try {
+        const jsonString = await fs.readFile(jsonFilePath, "utf-8");
+        return JSON.parse(jsonString);
+    } catch (error) {
+        console.log(error);
+    }
 };
