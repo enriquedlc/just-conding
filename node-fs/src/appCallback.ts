3d59b6b6b095d8e490b56ec87d147271c4d96b0a
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-11-01 23:07:26 +0000

    :sparkles::refactor: main functions in app files

--- node-fs/src/appCallback.ts
+++ node-fs/src/appCallback.ts
@@ -1,15 +1,19 @@
 import { jsonToObjectCallback } from "./callbacks/jsonToObjectCallBack";
 import { objectToCSVCallback } from "./callbacks/objectToCSVCallback";
 
-jsonToObjectCallback("./src/data/products.json", (err, obteinedUsers) => {
-    if (err) {
-        console.log(err);
-    } else {
-        objectToCSVCallback(
-            obteinedUsers as unknown as any[],
-            ["article", "name", "price"],
-            "./src/exports/products.csv",
-            (data) => data.price > 7,
-        );
-    }
-});
+function main() {
+    jsonToObjectCallback("./src/data/products.json", (err, obteinedUsers) => {
+        if (err) {
+            console.log(err);
+        } else {
+            objectToCSVCallback(
+                obteinedUsers as unknown as any[],
+                ["article", "name", "price"],
+                "./src/exports/products.csv",
+                (data) => data.price > 7,
+            );
+        }
+    });
+}
+
+main();
--- node-fs/src/appPromise.ts
+++ node-fs/src/appPromise.ts
@@ -1,10 +1,14 @@
 import { jsonToObjectPromise } from "./promises/jsonToObjectPromise";
 import { objectToCSVPromise } from "./promises/objectToCSVPromise";
 
-jsonToObjectPromise("./src/data/products.json").then((products) => {
-    objectToCSVPromise(
-        products,
-        ["article", "category", "price", "stock"],
-        "./src/exports/products.csv",
-    );
-});
+function main() {
+    jsonToObjectPromise("./src/data/products.json").then((products) => {
+        objectToCSVPromise(
+            products,
+            ["article", "category", "price", "stock"],
+            "./src/exports/products.csv",
+        );
+    });
+}
+
+main();
