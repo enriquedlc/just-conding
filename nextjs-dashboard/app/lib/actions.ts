ceed0d48dfc9abd3807fa1c10c08e5c08945e042
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-11-05 17:21:54 +0000

    :sparkles: added authentication to the app

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,9 +1,10 @@
 "use server";
 
-import { z } from "zod";
+import { object, z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
 
 export type State = {
   errors?: {
@@ -106,5 +107,22 @@ export async function deleteInvoice(id: string) {
     return { message: "Deleted Invoice." };
   } catch (error) {
     return { message: "Database Error: Failed to Delete Invoice." };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    console.log("formData", formData);
+    const cred = await signIn("credentials", Object.fromEntries(formData));
+    console.log(cred);
+    redirect("/dashboard");
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialsSignin";
+    }
+    throw error;
   }
 }
